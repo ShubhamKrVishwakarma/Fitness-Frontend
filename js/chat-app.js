e815994f63d6d8de41de7a2d76509ff8e7ccd792
DhanrajDwivedi <dhanrajdwivedi2003@gmail.com>
2024-03-16 16:49:13 +0530

    crucial change in chat front-end

--- js/chat-app.js
+++ js/chat-app.js
@@ -1,62 +1,12 @@
-// start: Sidebar
-document.querySelector('.chat-sidebar-profile-toggle').addEventListener('click', function(e) {
-    e.preventDefault()
-    this.parentElement.classList.toggle('active')
-})
+function StartChat(id){
+    document.getElementById('chatPanel').removeAttribute('style');
+    document.getElementById('divStart').removeAttribute('style','display:none');
 
-document.addEventListener('click', function(e) {
-    if(!e.target.matches('.chat-sidebar-profile, .chat-sidebar-profile *')) {
-        document.querySelector('.chat-sidebar-profile').classList.remove('active')
-    }
-})
-// end: Sidebar
+    document.getElementById('side-1').classList.add('d-none', 'd-md-block');
+    document.getElementById('side-2').classList.remove('d-none');
+}
 
-
-
-// start: Coversation
-document.querySelectorAll('.conversation-item-dropdown-toggle').forEach(function(item) {
-    item.addEventListener('click', function(e) {
-        e.preventDefault()
-        if(this.parentElement.classList.contains('active')) {
-            this.parentElement.classList.remove('active')
-        } else {
-            document.querySelectorAll('.conversation-item-dropdown').forEach(function(i) {
-                i.classList.remove('active')
-            })
-            this.parentElement.classList.add('active')
-        }
-    })
-})
-
-document.addEventListener('click', function(e) {
-    if(!e.target.matches('.conversation-item-dropdown, .conversation-item-dropdown *')) {
-        document.querySelectorAll('.conversation-item-dropdown').forEach(function(i) {
-            i.classList.remove('active')
-        })
-    }
-})
-
-document.querySelectorAll('.conversation-form-input').forEach(function(item) {
-    item.addEventListener('input', function() {
-        this.rows = this.value.split('\n').length
-    })
-})
-
-document.querySelectorAll('[data-conversation]').forEach(function(item) {
-    item.addEventListener('click', function(e) {
-        e.preventDefault()
-        document.querySelectorAll('.conversation').forEach(function(i) {
-            i.classList.remove('active')
-        })
-        document.querySelector(this.dataset.conversation).classList.add('active')
-    })
-})
-
-document.querySelectorAll('.conversation-back').forEach(function(item) {
-    item.addEventListener('click', function(e) {
-        e.preventDefault()
-        this.closest('.conversation').classList.remove('active')
-        document.querySelector('.conversation-default').classList.add('active')
-    })
-})
-// end: Coversation
+function showChatList(){
+    document.getElementById('side-1').classList.remove('d-none', 'd-md-block');
+    document.getElementById('side-2').classList.add('d-none');
+}
